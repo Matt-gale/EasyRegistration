9561573d3971766ce92fe53d84d8927da338c425
Stephen Prayogo <stephen_prayogo@dialog.com.au>
2017-07-20 16:56:19 +1000

    Kotlin setup and updated Gradle

--- EasyRegistrationAndroid/app/src/main/java/com/easyregistration/easyregistration/MainActivity.kt
+++ EasyRegistrationAndroid/app/src/main/java/com/easyregistration/easyregistration/MainActivity.kt
@@ -3,10 +3,13 @@ package com.easyregistration.easyregistration
 import android.app.Activity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello.setText("This is new text!")
     }
 }
