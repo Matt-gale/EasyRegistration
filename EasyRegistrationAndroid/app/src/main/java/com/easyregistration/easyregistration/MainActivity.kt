2b9280d2fb2277cf0190afd9c8ad3e746da4b4b7
Stephen Prayogo <stephen_prayogo@dialog.com.au>
2017-07-21 14:31:13 +1000

    Implemented basic login page

--- EasyRegistrationAndroid/app/src/main/java/com/easyregistration/easyregistration/MainActivity.kt
+++ EasyRegistrationAndroid/app/src/main/java/com/easyregistration/easyregistration/MainActivity.kt
@@ -10,6 +10,5 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hello.setText("This is new text!")
     }
 }
