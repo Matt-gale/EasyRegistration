69f0b143c0dbcb5490f550f7d3a396ca981cfce2
Ye He <yehe01@gmail.com>
2017-07-24 10:33:19 +1000

    Add main view controller after login

---
+++ EasyRegistrationIOS/EasyRegistrationIOS/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  EasyRegistrationIOS
+//
+//  Created by Ye He on 24/7/17.
+//
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
